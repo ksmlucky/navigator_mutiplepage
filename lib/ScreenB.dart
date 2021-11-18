07cb02e8cf2a5e1d70f1f55819c14eb2e36f471c
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-11-19 00:20:23 +0900

    ScreenB - StatefulWidget으로 바꿈. initState(), build(), dipose() 테스트

--- lib/ScreenB.dart
+++ lib/ScreenB.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
-class ScreenB extends StatelessWidget {
+class ScreenB extends StatefulWidget {
+  @override
+  _ScreenBState createState() => _ScreenBState();
+}
+
+class _ScreenBState extends State<ScreenB> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('initState is called');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print('dispose is called');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build is called');
+
     return Scaffold(
       appBar: AppBar(
         title: Text('ScreenB'),
